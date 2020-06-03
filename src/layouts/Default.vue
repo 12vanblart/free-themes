fb09b6111b728c6a7c310c6e2c286e364c8f4354
12vanblart <12vanblart@gmail.com>
2020-06-03 08:34:53 -0400

    ✨ Vuetify + Colorpicks w/ Sample Gradient

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,16 +1,16 @@
 <template>
-  <div class="layout">
-    <header class="header">
-      <strong>
-        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
-      </strong>
-      <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
-      </nav>
-    </header>
-    <slot/>
-  </div>
+  <v-app>
+    <v-app-bar app dense dark>
+      <v-toolbar-title>
+        {{ $static.metadata.siteName }}
+      </v-toolbar-title>
+    </v-app-bar>
+    <v-content>
+      <v-container fluid>
+        <slot />
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <static-query>
@@ -23,28 +23,7 @@ query {
 
 <style>
 body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
-  line-height: 1.5;
-}
-
-.layout {
-  max-width: 760px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
-
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
-}
-
-.nav__link {
-  margin-left: 20px;
+  font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto,
+    "Helvetica Neue", Arial, sans-serif;
 }
 </style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,33 +1,63 @@
 <template>
   <Layout>
-
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-
-    <h1>Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
-
+    <v-row>
+      <v-color-picker
+        class="mx-auto my-10"
+        mode="hexa"
+        show-swatches
+        :swatches="swatchOpts"
+        v-model="color1"
+      ></v-color-picker>
+      <v-color-picker
+        class="mx-auto my-10"
+        mode="hexa"
+        show-swatches
+        :swatches="swatchOpts"
+        v-model="color2"
+      ></v-color-picker>
+    </v-row>
+    <v-row>
+      <v-spacer></v-spacer>
+      <v-sheet
+        width="300"
+        height="300"
+        elevation="10"
+        class=""
+        :style="{
+          background:
+            'linear-gradient(135deg, ' + color1 + ' 0%, ' + color2 + ' 100%)',
+        }"
+      >
+      </v-sheet>
+      <v-spacer></v-spacer>
+    </v-row>
   </Layout>
 </template>
 
 <script>
 export default {
   metaInfo: {
-    title: 'Hello, world!'
-  }
-}
+    title: "Free Color Gradients",
+  },
+  data() {
+    return {
+      swatchOpts: [
+        ["#283555", "#3C8E50"],
+        ["#7746AF", "#0DE5F7"],
+        ["#FC8A87", "#FCE7C3"],
+        ["#665BA5", "#CBADE9"],
+        ["#E885A0", "#CCD4CC"],
+        ["#FFE3E8", "#94A9ED"],
+        ["#E27861", "#89DEB9"],
+        ["#830C20", "#FEF3E2"],
+        ["#F70D61", "#6EFCB5"],
+        ["#927FA0", "#BAECD2"],
+      ],
+      color1: "#C7FFE2",
+      color2: "#716799",
+    };
+  },
+};
 </script>
 
-<style>
-.home-links a {
-  margin-right: 1rem;
-}
-</style>
+<style></style>
