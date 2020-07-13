a9dff3207d406e3d521890c12412b64c6190b8b7
12vanblart <12vanblart@gmail.com>
2020-07-13 11:52:06 -0400

    ✨ Import Vuetify/framework for perf++?

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
-import Vuetify from "vuetify";
+import Vuetify from "vuetify/lib/framework";
 import "vuetify/dist/vuetify.min.css";
 import DefaultLayout from "~/layouts/Default.vue";
 
