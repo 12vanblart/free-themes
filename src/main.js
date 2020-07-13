7d1f022ca4f886bd4448153b234e5d1bba95fa0b
12vanblart <12vanblart@gmail.com>
2020-07-13 11:51:02 -0400

    ❌ Take out the trash

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ export default function(Vue, { appOptions, router, head, isClient }) {
   head.link.push({
     rel: "stylesheet",
     href:
-      "https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap",
+      "https://fonts.googleapis.com/css2?family=Roboto:wght@400&display=swap",
   });
 
   const opts = {};
